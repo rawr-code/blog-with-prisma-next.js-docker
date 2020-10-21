f23ba3bcf6eaf44cff474c228023ebce1b9e81e9
Emmanuel Villegas Gonzalez <emmanuelvillegasgonzalez@gmail.com>
2020-10-20 20:14:11 -0400

    docker for backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,9 @@ import { ApolloServer } from "apollo-server";
 import { schema } from "./schema";
 import { createContext } from "./context";
 
+const PORT = 4000;
+
 new ApolloServer({ schema, context: createContext }).listen(
-  { port: 4000 },
-  () =>
-    console.log(
-      `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-apollo-server#using-the-graphql-api`
-    )
+  { port: PORT },
+  () => console.log(`🚀 Prisma is ready at: http://localhost:${PORT}`)
 );
